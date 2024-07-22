00e717ec43384dd1318e74a15854d20ded39fea5
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2024-07-22 17:48:03 +0200

    Update OpenRelanceDataBase.sql

--- DataBase/OpenRelanceDataBase.sql
+++ DataBase/OpenRelanceDataBase.sql
@@ -11,24 +11,54 @@ CREATE TABLE user_open_relance (
     prenom_user_open_relance VARCHAR(255) NOT NULL,
     initial_user_open_relance CHAR(2) NOT NULL,
     type_de_profil INT NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL -- Ajout du champ mot_de_passe
+    email_user_open_relance VARCHAR(255) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL
 );
 
 -- Création de la table clients
 CREATE TABLE clients (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom_client VARCHAR(255) NOT NULL,
-    numeros_parma VARCHAR(255) NOT NULL
+    numeros_parma VARCHAR(255) NOT NULL,
+    id_user_open_relance INT,
+    FOREIGN KEY (id_user_open_relance) REFERENCES user_open_relance(id)
+);
+
+-- Création de la table contactes_clients
+CREATE TABLE contactes_clients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fonction_contactes_clients VARCHAR(255) NOT NULL,
+    nom_contactes_clients VARCHAR(255) NOT NULL,
+    mail_contactes_clients VARCHAR(255) NOT NULL UNIQUE,
+    telphone_contactes_clients VARCHAR(20) NOT NULL,
+    id_clients INT,
+    FOREIGN KEY (id_clients) REFERENCES clients(id)
+);
+
+-- Création de la table relance_client
+CREATE TABLE relance_client (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type_relance VARCHAR(255) NOT NULL, -- le type de relance (par ex: mail, appel, courrier 1, courrier 2, recommandé ...)
+    date_relance DATE NOT NULL, -- date de la relance
+    id_contact_client INT,
+    id_user_open_relance INT,
+    FOREIGN KEY (id_contact_client) REFERENCES contactes_clients(id),
+    FOREIGN KEY (id_user_open_relance) REFERENCES user_open_relance(id)
 );
 
 -- Création de la table factures
 CREATE TABLE factures (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    numeros_de_facture VARCHAR(255) NOT NULL,
+    numeros_de_facture VARCHAR(255) NOT NULL UNIQUE,
     date_echeance_payment DATE NOT NULL,
     montant_facture DECIMAL(10, 2) NOT NULL,
-    id_entreprise INT,
-    FOREIGN KEY (id_entreprise) REFERENCES clients(id)
+    montant_reste_a_payer DECIMAL(10, 2) NOT NULL,
+    id_clients INT,
+    id_relance_client INT,
+    id_user_open_relance INT,
+    FOREIGN KEY (id_clients) REFERENCES clients(id),
+    FOREIGN KEY (id_relance_client) REFERENCES relance_client(id),
+    FOREIGN KEY (id_user_open_relance) REFERENCES user_open_relance(id)
 );
 
 -- Création de la table commentaires
@@ -37,16 +67,7 @@ CREATE TABLE commentaires (
     date_commentaire TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     message_commentaire TEXT NOT NULL,
     id_factures INT,
-    FOREIGN KEY (id_factures) REFERENCES factures(id)
-);
-
--- Création de la table contactes_clients
-CREATE TABLE contactes_clients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    fonction_contactes_clients VARCHAR(255) NOT NULL,
-    nom_contactes_clients VARCHAR(255) NOT NULL,
-    mail_contactes_clients VARCHAR(255) NOT NULL,
-    telphone_contactes_clients VARCHAR(20) NOT NULL,
-    id_entreprise INT,
-    FOREIGN KEY (id_entreprise) REFERENCES clients(id)
+    id_user_open_relance INT,
+    FOREIGN KEY (id_factures) REFERENCES factures(id),
+    FOREIGN KEY (id_user_open_relance) REFERENCES user_open_relance(id)
 );
